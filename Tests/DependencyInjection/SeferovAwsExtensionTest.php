b248e15a6bd82917b50a49d63e626c41c6272fdd
seferov <farhad.safarov@gmail.com>
2014-06-08 12:03:07 +0300

    Simple tests added

---
+++ Tests/DependencyInjection/SeferovAwsExtensionTest.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * This file is part of the SeferovAwsBundle package.
+ *
+ * (c) Farhad Safarov <http://ferhad.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Seferov\AwsBundle\Tests\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Seferov\AwsBundle\DependencyInjection\SeferovAwsExtension;
+
+/**
+ * Class SeferovAwsExtensionTest
+ * @package Seferov\AwsBundle\Tests\DependencyInjection
+ *
+ * @author Farhad Safarov <http://ferhad.in>
+ */
+class SeferovAwsExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    private $container;
+
+    /**
+     * @var \Seferov\AwsBundle\DependencyInjection\SeferovAwsExtension
+     */
+    private $extension;
+
+    /**
+     * @var array
+     */
+    private static $config = array(
+        array(
+            'key' => 'SOME_KEY',
+            'secret' => 'SOME_SECRET'
+        )
+    );
+
+    /**
+     * @var array
+     */
+    private static $services = array('s3', 'sqs');
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->container = new ContainerBuilder();
+        $this->extension = new SeferovAwsExtension();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        unset($this->container, $this->extension);
+    }
+
+    public function testServices()
+    {
+        $this->extension->load(self::$config, $this->container);
+
+        foreach (self::$services as $service) {
+           $this->assertTrue($this->container->hasDefinition('aws.'.$service));
+        }
+    }
+}
